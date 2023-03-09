27d9741d9bf90ca2afc7fa08810d3863074030fe
thang5878 <thang5878@gmail.com>
2023-03-09 17:33:07 +0700

    minor fix

---
+++ QLCB/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        QLCB qlcb = new QLCB();
+        System.out.println("Chương trình quản lý cán bộ (Java)");
+        System.out.println("------------------------------------\n");
+        qlcb.hienThiManHinhChinh();
+    }
+}
