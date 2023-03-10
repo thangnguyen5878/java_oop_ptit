b85880d3b3b3726cc68a6f6b7b897e7483b2abea
thang5878 <thang5878@gmail.com>
2023-03-10 11:08:19 +0700

    QLCB project: add store data in data.csv

--- QLCB/src/Main.java
+++ QLCB/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         QLCB qlcb = new QLCB();
-        System.out.println("Chương trình quản lý cán bộ (Java)");
-        System.out.println("------------------------------------\n");
-        qlcb.hienThiManHinhChinh();
     }
 }
--- QLCB/src/QLCB.java
+++ QLCB/src/QLCB.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -6,46 +7,45 @@ import static java.lang.System.exit;
 
 public class QLCB {
     private List<CanBo> danhSachCanBo;
-    private Scanner scanner;
+    private String filename = "./src/data.csv";
+    Scanner scanner;
 
     public QLCB() {
         danhSachCanBo = new ArrayList<>();
         scanner = new Scanner(System.in);
+        readData(filename);
+        System.out.println("Chương trình quản lý cán bộ (Java)");
+        System.out.println("------------------------------------\n");
+        hienThiManHinhChinh();
     }
 
     public void themCanBo() {
-        System.out.printf("Thêm cán bộ:\n");
+        System.out.print("Thêm cán bộ:\n");
         System.out.println("1. Công nhân");
         System.out.println("2. Kỹ sư");
         System.out.println("3. Nhân viên");
 
-        int choice = 0;
+        int choice;
         while (true) {
             System.out.println("Nhập lựa chọn của bạn (1-3): ");
             choice = scanner.nextInt();
             scanner.nextLine();
-            if(choice >= 1 || choice <= 3)
+            if (choice >= 1 && choice <= 3)
                 break;
             System.out.println("Lựa chọn không hợp lệ!");
         }
 
 
         switch (choice) {
-            case 1:
-                themCongNhan();
-                break;
-            case 2:
-                themKySu();
-                break;
-            case 3:
-                themNhanVien();
-                break;
+            case 1 -> themCongNhan();
+            case 2 -> themKySu();
+            case 3 -> themNhanVien();
         }
         troLaiManHinhChinh();
     }
 
     private void themCongNhan() {
-        System.out.printf("Nhập thông tin công nhân:\n");
+        System.out.print("Nhập thông tin công nhân:\n");
         System.out.println("Họ tên: ");
         String hoTen = scanner.nextLine();
         System.out.println("Tuổi: ");
@@ -54,7 +54,7 @@ public class QLCB {
 
         // Nhập giới tính
         int temp;
-        String gioiTinh = "";
+        String gioiTinh;
         while (true) {
             System.out.println("Nhập giới tính (Nam [0], Nữ [1]): ");
             temp = scanner.nextInt();
@@ -67,7 +67,7 @@ public class QLCB {
             } else {
                 System.out.println("Giới tính không hợp lệ!");
             }
-        };
+        }
 
         // Nhập địa chỉ
         System.out.println("Nhập địa chỉ: ");
@@ -80,19 +80,20 @@ public class QLCB {
             System.out.println("Cấp bậc (1-10): ");
             bac = scanner.nextInt();
             scanner.nextLine();
-            if(bac >= 1 || bac <= 10)
+            if (bac >= 1 && bac <= 10)
                 break;
             System.out.println("Cấp bậc không hợp lệ!");
         }
 
         CanBo congNhan = new CongNhan(hoTen, tuoi, gioiTinh, diaChi, bac);
         danhSachCanBo.add(congNhan);
+        writeData(congNhan, filename);
 
         System.out.println("Thêm công nhân thành công!");
     }
 
     private void themKySu() {
-        System.out.printf("Nhập thông tin kỹ sư:\n");
+        System.out.print("Nhập thông tin kỹ sư:\n");
         System.out.println("Họ tên: ");
         String hoTen = scanner.nextLine();
         System.out.println("Tuổi: ");
@@ -101,7 +102,7 @@ public class QLCB {
 
         // Nhập giới tính
         int temp;
-        String gioiTinh = "";
+        String gioiTinh;
         while (true) {
             System.out.println("Nhập giới tính (Nam [0], Nữ [1]): ");
             temp = scanner.nextInt();
@@ -114,7 +115,7 @@ public class QLCB {
             } else {
                 System.out.println("Giới tính không hợp lệ!");
             }
-        };
+        }
 
         // Nhập địa chỉ
         System.out.println("Nhập địa chỉ: ");
@@ -126,12 +127,13 @@ public class QLCB {
 
         CanBo kySu = new KySu(hoTen, tuoi, gioiTinh, diaChi, nganhDaoTao);
         danhSachCanBo.add(kySu);
+        writeData(kySu, filename);
 
         System.out.println("Thêm kỹ sư thành công!");
     }
 
     private void themNhanVien() {
-        System.out.printf("Nhập thông tin nhân viên:\n");
+        System.out.print("Nhập thông tin nhân viên:\n");
         System.out.println("Họ tên: ");
         String hoTen = scanner.nextLine();
         System.out.println("Tuổi: ");
@@ -140,7 +142,7 @@ public class QLCB {
 
         // Nhập giới tính
         int temp;
-        String gioiTinh = "";
+        String gioiTinh;
         while (true) {
             System.out.println("Nhập giới tính (Nam [0], Nữ [1]): ");
             temp = scanner.nextInt();
@@ -153,7 +155,7 @@ public class QLCB {
             } else {
                 System.out.println("Giới tính không hợp lệ!");
             }
-        };
+        }
 
         // Nhập địa chỉ
         System.out.println("Nhập địa chỉ: ");
@@ -165,6 +167,7 @@ public class QLCB {
 
         CanBo nhanVien = new NhanVien(hoTen, tuoi, gioiTinh, diaChi, congViec);
         danhSachCanBo.add(nhanVien);
+        writeData(nhanVien, filename);
 
         System.out.println("Thêm nhân viên thành công!");
     }
@@ -194,14 +197,14 @@ public class QLCB {
             }
         }
         // TH2: Duyệt hết danh sách nhưng không tìm thấy
-        if (found == false)
+        if (!found)
             System.out.println("Không tìm thấy cán bộ có họ tên là " + hoTen);
         troLaiManHinhChinh();
     }
 
     // Hiển thị danh sách cán bộ (chi tiết)
     public void hienThiDanhSach() {
-        if (danhSachCanBo.isEmpty() == true) {
+        if (danhSachCanBo.isEmpty()) {
             System.out.println("(trống)");
         } else {
             System.out.println("Danh sách cán bộ");
@@ -233,32 +236,95 @@ public class QLCB {
 
     // Màn hình chính (Home)
     public void hienThiManHinhChinh() {
-        System.out.printf("Nhập lựa chọn của bạn (1-4)\n");
+        System.out.print("Nhập lựa chọn của bạn (1-4)\n");
 
-        System.out.printf("1. Thêm cán bộ\n");
-        System.out.printf("2. Tìm kiếm theo họ tên\n");
-        System.out.printf("3. Hiển thị danh sách cán bộ\n");
-        System.out.printf("4. Thoát\n");
+        System.out.print("1. Thêm cán bộ\n");
+        System.out.print("2. Tìm kiếm theo họ tên\n");
+        System.out.print("3. Hiển thị danh sách cán bộ\n");
+        System.out.print("4. Thoát\n");
 
         int choice;
         while (true) {
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
-                case 1:
-                    themCanBo();
-                    break;
-                case 2:
-                    timKiem();
-                    break;
-                case 3:
-                    hienThiDanhSach();
-                    break;
-                case 4:
-                    exit(0);
-                default:
-                    System.out.println("Lựa chọn không hợp lệ");
+                case 1 -> themCanBo();
+                case 2 -> timKiem();
+                case 3 -> hienThiDanhSach();
+                case 4 -> exit(0);
+                default -> System.out.println("Lựa chọn không hợp lệ");
             }
+        }
+
+    }
+
+    public void readData(String filename) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                String type = fields[0].trim();
+                switch (type) {
+                    case "Công nhân" -> {
+                        String hoTen = fields[1].trim();
+                        int tuoi = Integer.parseInt(fields[2].trim());
+                        String gioiTinh = fields[3].trim();
+                        String diaChi = fields[4].trim();
+                        int bac = Integer.parseInt(fields[5].trim());
+                        CanBo congNhan = new CongNhan(hoTen, tuoi, gioiTinh, diaChi, bac);
+                        danhSachCanBo.add(congNhan);
+                    }
+                    case "Kỹ sư" -> {
+                        String hoTen = fields[1].trim();
+                        int tuoi = Integer.parseInt(fields[2].trim());
+                        String gioiTinh = fields[3].trim();
+                        String diaChi = fields[4].trim();
+                        String nganhDaoTao = fields[5].trim();
+                        CanBo kySu = new KySu(hoTen, tuoi, gioiTinh, diaChi, nganhDaoTao);
+                        danhSachCanBo.add(kySu);
+                    }
+                    case "Nhân viên" -> {
+                        String hoTen = fields[1].trim();
+                        int tuoi = Integer.parseInt(fields[2].trim());
+                        String gioiTinh = fields[3].trim();
+                        String diaChi = fields[4].trim();
+                        String congViec = fields[5].trim();
+                        CanBo nhanVien = new NhanVien(hoTen, tuoi, gioiTinh, diaChi, congViec);
+                        danhSachCanBo.add(nhanVien);
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+            System.out.println("Data loaded successfully.");
+        } catch (IOException e) {
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+    }
+
+    private void writeData(CanBo canBo, String filename) {
+        try {
+            FileWriter fileWriter = new FileWriter(filename, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            if (canBo instanceof CongNhan) {
+                String line = "Công nhân" + ", " + canBo.getHoTen() + ", " + canBo.getTuoi() + ", " + canBo.getGioiTinh() + ", " + canBo.getDiaChi() + ", " + ((CongNhan) canBo).getBac();
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            } else if (canBo instanceof KySu) {
+                String line = "Kỹ sư" + ", " + canBo.getHoTen() + ", " + canBo.getTuoi() + ", " + canBo.getGioiTinh() + ", " + canBo.getDiaChi() + ", " + ((KySu) canBo).getNganhDaoTao();
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            } else if (canBo instanceof NhanVien) {
+                String line = "Nhân viên" + ", " + canBo.getHoTen() + ", " + canBo.getTuoi() + ", " + canBo.getGioiTinh() + ", " + canBo.getDiaChi() + ", " + ((NhanVien) canBo).getCongViec();
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
